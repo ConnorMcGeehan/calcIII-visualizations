7f56980ed404c76bc86af981835f282d8948c71f
Connor McGeehan <cmcgeehan2421@gmail.com>
2025-12-13 16:00:53 -0500

    Implemented plot

--- src/level_curves.py
+++ src/level_curves.py
@@ -1,23 +1,27 @@
 from base_plot import BasePlot
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import Optional
 
 
 class LevelCurves(BasePlot):
-    """Level curves plotter.
 
-    Inherits from `BasePlot`
-    """
+    def __init__(self):
+        super().__init__()
+        self.x = self.X
+        self.y = self.Y
+        self.z = self.Z
 
     def plot(self):
         """
         Draw level curves for the loss function/grid computed by `BasePlot`.
         :returns: The level curves plot for the function defined in `BasePlot`
         """
-        pass
+        fig, ax = plt.subplots()
+        CS = ax.contour(self.x, self.y, self.z)
+        ax.clabel(CS, fontsize=10)
+        plt.show()
 
-    def update_plot(self, ax: Optional[plt.Axes] = None, **kw):
+    def update_plot(self):
         """
         Convenience wrapper to redraw/update the level curves on when weights or
         alpha are updated.
