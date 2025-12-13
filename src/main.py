04a9509d58a097e73669f08adb2e37efbe867daa
Connor McGeehan <cmcgeehan2421@gmail.com>
2025-12-13 12:43:50 -0500

    Skeleton code for Main

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import numpy as np
+from base_plot import BasePlot
+
+
+class Main():
+
+    def __init__(self):
+        self.data_list = None
+
+    def get_data():
+        """
+        Parses nationals_data.csv and stores the relevant rows/columns
+        as `DataPoint` objects.
+        :post: self.data_list is populated with `DataPoint` objects
+        """
+        pass
+    
+    def run():
+        """
+        The function that runs the project
+        """
+        pass
+
+
+
+Main.run()
